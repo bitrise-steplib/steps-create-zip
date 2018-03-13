7f887510b5088a61fbff7765bff3de52e0ea60f7
Akos Birmacher <akos.birmacher@bitrise.io>
2018-03-13 16:42:02 +0100

    Minimal unit tests

---
+++ main_test.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fixTargetExtension(t *testing.T) {
+	type args struct {
+		targetPath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "dir",
+			args: args{"dir"},
+			want: "dir.zip",
+		},
+		{
+			name: "nested_dir",
+			args: args{"/dir/nestedDir"},
+			want: "/dir/nestedDir.zip",
+		},
+		{
+			name: "withExtension",
+			args: args{"dir.zip"},
+			want: "dir.zip",
+		},
+		{
+			name: "file",
+			args: args{"file.text"},
+			want: "file.text.zip",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fixTargetExtension(tt.args.targetPath); got != tt.want {
+				t.Errorf("fixTargetExtension() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_createConfigsModelFromEnvs(t *testing.T) {
+	tests := []struct {
+		name string
+		want ConfigsModel
+	}{
+		{
+			name: "",
+			want: ConfigsModel{"", ""},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := createConfigsModelFromEnvs(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("createConfigsModelFromEnvs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConfigsModel_validate(t *testing.T) {
+	type fields struct {
+		SourcePath string
+		TargetPath string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name:    "file",
+			fields:  fields{"./", "./"},
+			wantErr: false,
+		},
+		{
+			name:    "not_exist",
+			fields:  fields{"./folder", "./"},
+			wantErr: true,
+		},
+		{
+			name:    "sourcPath_empty",
+			fields:  fields{"", "./folder"},
+			wantErr: true,
+		},
+		{
+			name:    "targetPath_empty",
+			fields:  fields{"./folder", ""},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			configs := ConfigsModel{
+				SourcePath: tt.fields.SourcePath,
+				TargetPath: tt.fields.TargetPath,
+			}
+			if err := configs.validate(); (err != nil) != tt.wantErr {
+				t.Errorf("ConfigsModel.validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_validateTargetPath(t *testing.T) {
+	type args struct {
+		targetPath string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "empty",
+			args: args{""},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			validateTargetPath(tt.args.targetPath)
+		})
+	}
+}
